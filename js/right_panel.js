5a28560442499b03384968b19bb2036c225dce9f
Despair <lfp002@yandex.ru>
2015-10-07 22:47:04 +0500

    send json to server

--- js/right_panel.js
+++ js/right_panel.js
@@ -10,10 +10,71 @@ function create_right_panel() {
 	var s=game.make.sprite(10,20,'buttons',0);
 	s.inputEnabled=true;
 	s.input.priorityID = 1;
-	s.events.onInputDown.add(onNetDown,this);
+	s.events.onInputDown.add(sendJson,this);
+	r.addChild(s);
+	s=game.make.sprite(80,20,'buttons',1);
+	s.inputEnabled=true;
+	s.input.priorityID = 1;
+	s.events.onInputDown.add(onLoadUnits,this);
+	r.addChild(s);
+	s=game.make.sprite(150,20,'buttons',2);
+	s.inputEnabled=true;
+	s.input.priorityID = 1;
+	s.events.onInputDown.add(del_poly,this);
 	r.addChild(s);
 	return r;
 	
+}
+
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+        	console.log(cookies[i])
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function sendJson() {
+	//alert(csrftoken)
+	//var a=document.getElementsByName('csrfmiddlewaretoken')[0].value
+	var csrftoken = getCookie('csrftoken');
+	$.ajaxSetup({
+	    beforeSend: function(xhr, settings) {
+	        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+	            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+	        }
+	    }
+	});
+	//alert(getCookie('csrftoken'))
+	var arr = { City: 'Moscow', Age: 25 };
+	$.ajax({
+	    url: host+'send.json',
+	    type: 'POST',
+	    data: JSON.stringify(arr),
+	    contentType: 'application/json; charset=utf-8',
+	    dataType: 'json',
+	    async: true,
+	    success: sendComplete
+	});
+}
+function sendComplete(msg) {
+	console.log(msg)
+	alert("hello")
 }
 
 function onNetDown() {
---
+++ js/show_units.js
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+var unit_graphics;
+var units_color=0xff33ff
+var units_own_color=0xff33ff
+var units_xofs=5
+var units_yofs=5
+var units_width=3
+function draw_poly () {
+	poly = new Phaser.Polygon(100,200,103,200,103,400,100,400)
+	unit_graphics=game.add.graphics(0,0)
+	//unit_graphics.beginFill(0xFF33ff)
+	unit_graphics.beginFill(units_color)
+	unit_graphics.drawPolygon(poly.points);
+	unit_graphics.endFill();
+}
+
+function onLoadUnits() {
+	console.log("loading units");
+	game.load.json("units_json",host+"units.json",true)
+	game.load.start();
+	game.load.onLoadComplete.add(onUnitsComplete,this)
+	//var pj=game.cache.getJSON("test1")
+	
+}
+function onUnitsComplete() {
+	var pj=game.cache.getJSON("units_json");
+	var max=pj.max
+	var yofs=(tile_size-2*units_yofs)/max
+	unit_graphics=game.add.graphics(0,0)
+	unit_graphics.beginFill(units_own_color)
+	for (i in pj.own){
+		var len1=pj.own[i][2]
+		if (len1<max/7) {
+			len1=max/7
+		}
+		len1=len1*yofs
+		var poly=new Phaser.Polygon(
+				pj.own[i][0]*tile_size+units_xofs,
+				pj.own[i][1]*tile_size+units_yofs,
+				pj.own[i][0]*tile_size+units_xofs+units_width,
+				pj.own[i][1]*tile_size+units_yofs,
+				pj.own[i][0]*tile_size+units_xofs+units_width,
+				//pj.own[i][1]*tile_size+units_yofs+yofs*pj.own[i][2],
+				pj.own[i][1]*tile_size+units_yofs+len1,
+				pj.own[i][0]*tile_size+units_xofs,
+				//pj.own[i][1]*tile_size+units_yofs+yofs*pj.own[i][2]
+				pj.own[i][1]*tile_size+units_yofs+len1
+				)
+		unit_graphics.drawPolygon(poly.points);
+	}
+	unit_graphics.endFill();
+}
+
+function del_poly() {
+	unit_graphics.destroy()
+}
