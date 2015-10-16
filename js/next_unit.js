749697c130aadef7584f345885ad6d4bd26d4354
Despair <lfp002@yandex.ru>
2015-10-16 21:16:04 +0500

    move unit

--- js/next_unit.js
+++ js/next_unit.js
@@ -31,5 +31,23 @@ function nextUnit() {
 		if (unit_cursor>=own_units_pos.length) { unit_cursor=0}
 		marker.x = own_units_pos[unit_cursor][1] * tile_size;
 		marker.y = own_units_pos[unit_cursor][2] * tile_size;
+		this.layer.events.onInputDown.removeAll(this)
+		this.layer.events.onInputDown.add(moveUnitbyTileDown,this);
 	}
+}
+
+function moveUnitbyTileDown() {
+	var x =this.layer.getTileX(game.input.activePointer.worldX);
+	var y = this.layer.getTileY(game.input.activePointer.worldY);
+	var arr = { x: x, y: y, id : own_units_pos[unit_cursor][0] };
+	$.ajax({
+	    url: host+'move_unit.json',
+	    type: 'POST',
+	    data: JSON.stringify(arr),
+	    contentType: 'application/json; charset=utf-8',
+	    dataType: 'json',
+	    async: true,
+	    success: function () {}
+	});
+	this.layer.events.onInputDown.removeAll(this)
 }
